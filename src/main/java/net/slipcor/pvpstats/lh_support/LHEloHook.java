8f18a03ef5042dd99dcb6c65b97a27bf01599ebb
caoli5288 <caoli5288@gmail.com>
2018-07-22 01:58:57 +0800

    Update LHEloHook.java

--- src/main/java/net/slipcor/pvpstats/lh_support/LHEloHook.java
+++ src/main/java/net/slipcor/pvpstats/lh_support/LHEloHook.java
@@ -1,35 +1,21 @@
 package net.slipcor.pvpstats.lh_support;
 
-import com.google.common.collect.Maps;
-import lombok.val;
-import me.robin.leaderheads.api.LeaderHeadsAPI;
-import me.robin.leaderheads.datacollectors.DataCollector;
+import me.robin.leaderheads.datacollectors.OnlineDataCollector;
 import me.robin.leaderheads.objects.BoardType;
 import net.slipcor.pvpstats.PVPData;
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 
-public class LHEloHook extends DataCollector {
+public class LHEloHook extends OnlineDataCollector {
 
     public LHEloHook(Plugin plugin) {
-        super("i5mc-pvp-elo", plugin.getName(), BoardType.DEFAULT, "", "i5mc-pvp-elo", Arrays.asList(null, null, "{amount}", null), true, UUID.class);
+        super("i5mc-pvp-elo", plugin.getName(), BoardType.DEFAULT, "i5mc-pvp-elo", "", Arrays.asList(null, null, "{amount}"));
     }
 
     @Override
-    public List<Map.Entry<?, Double>> requestAll() {
-        val out = Maps.<Object, Double>newHashMap();
-        val itr = Bukkit.getOnlinePlayers().iterator();
-        Player p;
-        while (itr.hasNext()) {
-            p = itr.next();
-            out.put(p.getUniqueId(), PVPData.getEloScore(p).doubleValue());
-        }
-        return LeaderHeadsAPI.sortMap(out);
+    public Double getScore(Player player) {
+        return PVPData.getEloScore(player).doubleValue();
     }
 }
